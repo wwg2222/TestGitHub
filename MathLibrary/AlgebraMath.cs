1d6028037e71d716dbc8739c1a105fa963c47b23
wengangwang <wwang@thorlabs.com>
2017-10-19 17:58:14 +0800

    Test test application

--- MathLibrary/AlgebraMath.cs
+++ MathLibrary/AlgebraMath.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace MathLibrary
 {
     public class AlgebraMath
     {
-        public int Add(int x, int y)
+        public static  int Add(int x, int y)
         {
             return x + y;
         }
@@ -16,6 +17,15 @@ namespace MathLibrary
         public int Multipy(int x, int y)
         {
             return x*y;
+        }
+    }
+
+    public class TestAlgebra
+    {
+        [Fact]
+        public void TestAdd()
+        {
+            Assert.Equal(2, AlgebraMath.Add(1,1));
         }
     }
 }
